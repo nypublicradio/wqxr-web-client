3af59e0246570b5817524a647c146033b8ba90b1
Brian Whitton <me@brianwhitton.com>
2017-02-21 23:38:51 -0500

    rebase cleanups

--- app/instance-initializers/link-handler.js
+++ app/instance-initializers/link-handler.js
@@ -40,7 +40,7 @@ function _trackEvent(data, instance) {
   if (model) {
     eventToTrack.model = model;
   }
-  metrics.trackEvent(eventToTrack);
+  metrics.trackEvent('GoogleAnalytics', eventToTrack);
 }
 
 function _trackLegacyEvent(event, instance) {
@@ -117,7 +117,7 @@ export default {
 
         let { routeName, params, queryParams } = router.recognize(href);
         router.transitionTo(routeName, params, queryParams);
-        preventDefault.bind(event);
+        preventDefault.bind(event)();
         return false;
       } else if (isExternal && !Ember.testing) {
         $target.attr('target', '_blank');
