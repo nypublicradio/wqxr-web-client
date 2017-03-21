56f86579c200b8f69ef133555fbd1e377777b6ed
Michael Hearn <git@mikehearn.net>
2017-03-21 13:24:51 -0400

    Removed membership env var, using wnycAPI val

--- mirage/config.js
+++ mirage/config.js
@@ -193,5 +193,5 @@ export default function() {
   /*-------------------------------------------------------------
   membership microservice
   ---------------------------------------------------------------*/
-  this.get('${config.wnycAuthAPI}/membership/v1/orders', 'orders');
+  this.get('${config.wnycAPI}/membership/v1/orders/', 'orders');
 }
