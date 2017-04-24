86c083bf8c95ce71df59faa0c77d97dbd319c078
Brian Whitton <me@brianwhitton.com>
2017-04-24 17:07:37 -0400

    trying to turn bucketItem literals into ember data instances

--- app/bucket/serializer.js
+++ app/bucket/serializer.js
@@ -3,8 +3,15 @@ import JSONAPISerializer from 'ember-data/serializers/json-api';
 export default JSONAPISerializer.extend({
   keyForAttribute(key) { return key; },
   modelNameFromPayloadKey() { return 'bucket'; },
-  normalizeResponse(store, klass, payload/*, id, requestType*/) {
-    payload.data.id = payload.data.attributes.slug;
-    return this._super(...arguments);
+  extractId(model, {attributes}) {
+    return attributes.slug;
+  },
+  normalizeResponse(store, klass, payload, id, requestType) {
+    let { bucketItems = [] } = payload.data.attributes;
+    // bucketItems.map(item => store.push({data: item}));
+    payload.data.relationships.bucketItems = {data: bucketItems.map(b => ({id: b.id, type: b.type}))};
+    payload.included = bucketItems;
+    delete payload.data.attributes.bucketItems;
+    return this._super(store, klass, payload, id, requestType);
   }
 });
