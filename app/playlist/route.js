50544e46afe56b2b3cc0bf8c3b3807f1848c5d1c
Brian Whitton <me@brianwhitton.com>
2017-04-24 09:34:32 -0400

    updates page titles
    use correct property for streame name on playlist route
    don't say WNYC in application route

--- app/playlist/route.js
+++ app/playlist/route.js
@@ -1,12 +1,10 @@
 import Route from 'ember-route';
-import get from 'ember-metal/get';
 
 export default Route.extend({
   model({ slug }) {
     return this.store.findRecord('stream', slug);
   },
   titleToken(model) {
-    let streamTitle = get(model, 'streaminfo.name');
-    return `Playlist for ${streamTitle}`;
+    return `Playlist for ${model.get('name')}`;
   },
 });
