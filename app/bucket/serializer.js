1f423796a178528cc38248cb285a83ac20dfaad6
Brian Whitton <me@brianwhitton.com>
2017-05-02 23:09:29 -0400

    just return the key for models
    this was interfering with relationships

--- app/bucket/serializer.js
+++ app/bucket/serializer.js
@@ -2,13 +2,12 @@ import JSONAPISerializer from 'ember-data/serializers/json-api';
 
 export default JSONAPISerializer.extend({
   keyForAttribute(key) { return key; },
-  modelNameFromPayloadKey() { return 'bucket'; },
+  modelNameFromPayloadKey: key => key,
   extractId(model, {attributes}) {
     return attributes.slug;
   },
   normalizeResponse(store, klass, payload, id, requestType) {
     let { bucketItems = [] } = payload.data.attributes;
-    // bucketItems.map(item => store.push({data: item}));
     payload.data.relationships.bucketItems = {data: bucketItems.map(b => ({id: b.id, type: b.type}))};
     payload.included = bucketItems;
     delete payload.data.attributes.bucketItems;
