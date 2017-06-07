fbf730a5e3f9de1e4e6f42bf8e77a06d4bfdcad4
Brian Whitton <me@brianwhitton.com>
2017-06-07 17:50:54 -0400

    add `listing-page` to mirage
    we've been using `show` as a short hand for `listing-pages` for a while,
    but they have very different use cases. For now shows are just the
    things rendered on `/shows`, but `listing-pages` are a detail view on
    shows, which include the `api-response` abstraction and all that other
    stuff.
    
    this commit makes that distinction more explicit. in the future, shows
    will be able to fully encapsulate the sub routing, but for now, this
    works.

--- mirage/serializers/show.js
+++ mirage/serializers/show.js
@@ -1,17 +1,5 @@
-import ApplicationSerializer from './application';
-import { dasherize } from 'ember-string';
+import { JSONAPISerializer } from 'ember-cli-mirage';
 
-export default ApplicationSerializer.extend({
-  include: ['apiResponse'],
-  keyForAttribute: key => key,
-  keyForRelationship: key => dasherize(key),
-  typeKeyForModel(model) {
-    // This is a Mirage serialization bug, the wrong serializer
-    // is being used for 'story'.
-    if (model.modelName === 'show') {
-      return 'channel';
-    } else {
-      return model.modelName.dasherize();
-    }
-  },
+export default JSONAPISerializer.extend({
+  include: ['image']
 });
