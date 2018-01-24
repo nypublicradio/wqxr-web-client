8a607252e648fea0fe4d0b4d286685e86113ec3c
Michael Hearn <git@mikehearn.net>
2018-01-23 22:57:15 -0500

    reverses intentionally failing test from 3a970aa7

--- tests/acceptance/streams-test.js
+++ tests/acceptance/streams-test.js
@@ -6,7 +6,7 @@ moduleForAcceptance('Acceptance | streams');
 test('visiting /streams', function(assert) {
   let streams = server.createList('stream', 7);
   server.createList('whats-on', 7);
-  
+
   let wqxrStreams = streams.filter(stream => stream.attrs.source_tags.includes('wqxr_site'));
   let wnycStreams = streams.filter(stream => stream.attrs.source_tags.includes('wnyc_site'));
   let allStreams = wqxrStreams.sort((a, b) => a.site_priority - b.site_priority)
@@ -16,7 +16,7 @@ test('visiting /streams', function(assert) {
 
   andThen(function() {
     assert.equal(currentURL(), '/streams');
-    assert.equal(find('.stream-list li').length, 7, 'should display a list of streams');
+    assert.equal(find('.stream-list li').length, 6, 'should display a list of streams');
     assert.deepEqual(
       allStreams.map(stream => stream.attrs.name),
       find('.stream-list li .streamitem-header .streamitem-link').map((i, el) => el.textContent.trim()).get(),
@@ -41,7 +41,7 @@ test('playing a stream', function(assert) {
 test('stream routes do dfp targeting', function(/*assert*/) {
   server.createList('stream', 7);
   server.createList('whats-on', 7);
-  
+
   // https://github.com/emberjs/ember.js/issues/14716#issuecomment-267976803
   visit('/');
 
