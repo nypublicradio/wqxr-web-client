c3cfb1334b5532513cd185ca52db4a51e53e0426
Michael Hearn <git@mikehearn.net>
2018-01-23 23:08:05 -0500

    just full-out reverting streams-test

--- tests/acceptance/streams-test.js
+++ tests/acceptance/streams-test.js
@@ -4,24 +4,14 @@ import moduleForAcceptance from 'wqxr-web-client/tests/helpers/module-for-accept
 moduleForAcceptance('Acceptance | streams');
 
 test('visiting /streams', function(assert) {
-  let streams = server.createList('stream', 7);
+  server.createList('stream', 7);
   server.createList('whats-on', 7);
-
-  let wqxrStreams = streams.filter(stream => stream.attrs.source_tags.includes('wqxr_site'));
-  let wnycStreams = streams.filter(stream => stream.attrs.source_tags.includes('wnyc_site'));
-  let allStreams = wqxrStreams.sort((a, b) => a.site_priority - b.site_priority)
-    .concat(wnycStreams.sort((a, b) => a.site_priority - b.site_priority)).uniq();
-
+  
   visit('/streams');
 
   andThen(function() {
     assert.equal(currentURL(), '/streams');
     assert.equal(find('.stream-list li').length, 6, 'should display a list of streams');
-    assert.deepEqual(
-      allStreams.map(stream => stream.attrs.name),
-      find('.stream-list li .streamitem-header .streamitem-link').map((i, el) => el.textContent.trim()).get(),
-      'should order by site and priority'
-    );
   });
 });
 
@@ -41,7 +31,7 @@ test('playing a stream', function(assert) {
 test('stream routes do dfp targeting', function(/*assert*/) {
   server.createList('stream', 7);
   server.createList('whats-on', 7);
-
+  
   // https://github.com/emberjs/ember.js/issues/14716#issuecomment-267976803
   visit('/');
 
