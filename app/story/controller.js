a60c8c677c4a8523221fd743f56d55ed5588d213
Brian Whitton <me@brianwhitton.com>
2018-04-25 18:39:13 -0400

    this event is fired via click tracking in GTM now

--- app/story/controller.js
+++ app/story/controller.js
@@ -1,33 +1,19 @@
 import { scheduleOnce } from '@ember/runloop';
-import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
 
 export default Controller.extend({
   queryParams:  ['tab'],
-  tab: 'summary',
-  metrics: service(),
-  setTab(){
+  tab: null,
+
+  setTab() {
     if (location.hash.substr(1) === "transcript"){
-        this.set("tab", 'transcript');
-      }
+      this.set("tab", 'transcript');
+    }
   },
 
   init(){
     this._super(...arguments);
     scheduleOnce("afterRender", this, this.setTab);
   },
-
-  actions: {
-    onShowComments(story) {
-      let metrics = this.get('metrics');
-      let {containers:action, title:label} = story.get('analytics');
-
-      metrics.trackEvent('GoogleAnalytics', {
-        category: 'Displayed Comments',
-        action,
-        label
-      });
-    }
-  }
 });
