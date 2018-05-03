b03daa77952398e098dccf7166a80ff1d33f770a
Brian Whitton <me@brianwhitton.com>
2018-05-03 09:42:17 -0400

    update this test and pass in data-action

--- tests/acceptance/analytics-test.js
+++ tests/acceptance/analytics-test.js
@@ -26,7 +26,6 @@ module('Acceptance | Selectors for GTM Analytics', function(hooks) {
   test('test for analytics selectors', async function(assert) {
     setupHifi(this.owner);
     setBreakpoint('largeAndUp')
-    // registerWaiter(waitForStreams);
 
     server.create('bucket', {slug: 'wqxr-home'});
     server.createList('stream', 7);
@@ -34,21 +33,24 @@ module('Acceptance | Selectors for GTM Analytics', function(hooks) {
 
     await visit('/');
 
-    // unregisterWaiter(waitForStreams);
-
     assert.equal(currentURL(), '/');
     // UA, User clicks on a link in a homepage bucket
-    let show = findWithAssert('.brick__item .item__head__brand')[0];
     let overlayLink = findWithAssert('.brick__item .item__link--hidden')[0];
-    // let title = findWithAssert('.item__head__headline > a').first();
+    let title = findWithAssert('.item__head__headline > a')[0];
+    let show = findWithAssert('.brick__item .item__head__brand')[0];
 
     // assert event values
     let firstStory = server.db.buckets[0].bucketItems[0].attributes;
-    let shows = firstStory.headers.links.reverse().map(l => l.title).join(' | ');
 
-    assert.equal(`${shows} | ${firstStory.title}`, overlayLink.attributes.getNamedItem('title').value, 'link overlay should include analytics info');
-    // assert.equal(`${shows} | ${firstStory.title}`, title.attr('title'), 'headline link should include analytics info');
-    assert.equal(firstStory.headers.brand.title, show.attributes.getNamedItem('title').value, 'show link should include analytics info');
+    assert.equal(overlayLink.getAttribute('data-action'), 'Homepage', 'overlay link data-action');
+    assert.equal(overlayLink.getAttribute('data-label'), `${firstStory.title} | ${firstStory.headers.brand.title}`,'overlay link data-label');
+
+    assert.equal(title.getAttribute('data-action'), 'Homepage', 'story title data-action');
+    assert.equal(title.getAttribute('data-label'), `${firstStory.title} | ${firstStory.headers.brand.title}`, 'story title data-label');
+
+    assert.equal(show.getAttribute('data-action'), 'Homepage', 'show link data-action');
+    assert.equal(show.getAttribute('data-label'), `${firstStory.headers.brand.title}`, 'show link data-label');
+
     // UA, User Launches Stream from Stream Banner
     findWithAssert('.stream-banner__active-stream');
     await click('.stream-banner-listenbutton');
