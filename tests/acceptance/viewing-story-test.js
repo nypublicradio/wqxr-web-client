c8eb3a12f35339d249b7cee3dd4ce7e5a6a3ecf5
Brian Whitton <me@brianwhitton.com>
2018-05-08 17:04:51 -0400

    no click tracking!

--- tests/acceptance/viewing-story-test.js
+++ tests/acceptance/viewing-story-test.js
@@ -1,4 +1,4 @@
-import { findAll, currentURL, find, visit } from '@ember/test-helpers';
+import { currentURL, find, visit } from '@ember/test-helpers';
 import test from 'ember-sinon-qunit/test-support/test';
 import storyPage from 'wqxr-web-client/tests/pages/story';
 import config from 'wqxr-web-client/config/environment';
@@ -118,24 +118,6 @@ module('Acceptance |  Story Detail Analytics', function(hooks) {
       .withArgs(forDfp);
 
     await visit(`story/${story.slug}`);
-  });
-
-  test('listen button on story page includes data-action and data-label values', async function(assert) {
-    let story = server.create('story', {showTitle: 'foo show'});
-    let segmentStory = server.create('story', 'withSegments', {showTitle: 'foo show'});
-
-    await visit(`story/${story.slug}`);
-
-    let listenButton = find('#storyHeader [data-test-selector=listen-button]');
-    assert.equal(listenButton.getAttribute('data-action'), 'Clicked Play/Pause On Demand');
-    assert.equal(listenButton.getAttribute('data-label'), `${story.title} | foo show`);
-
-    await visit(`story/${segmentStory.slug}`);
-    let segmentButtons = findAll('#segmentsList [data-test-selector=listen-button]');
-    segmentButtons.forEach((el, i) => {
-      assert.equal(el.getAttribute('data-action'), 'Clicked Play/Pause On Demand');
-      assert.equal(el.getAttribute('data-label'), `${segmentStory.segments[i].title} | foo show`);
-    });
   });
 
   test('api request includes draft params', async function(assert) {
