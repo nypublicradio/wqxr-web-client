f2703b8c239a3ae4aac102dfb9d1071be76470ed
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2018-09-13 13:28:42 -0400

    increase poll time to 15s

--- app/routes/application.js
+++ app/routes/application.js
@@ -66,8 +66,12 @@ export default Route.extend(ApplicationRouteMixin, {
 
     window.WNYC_LEGACY_LOADER = get(this, 'legacyLoader');
 
-    let pollFunction = () => get(this, 'store').findAll('stream');
-    get(this, 'poll').addPoll({interval: 60 * 1000, callback: pollFunction});
+    let pollFunction = () => {
+      get(this, 'store').findAll('stream');
+
+    }
+
+    get(this, 'poll').addPoll({interval: 15 * 1000, callback: pollFunction});
   },
 
   model() {
