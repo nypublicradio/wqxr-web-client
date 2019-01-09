f9599fdc1cb159bb66b61a1e9f01a090d3fa9b5d
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-01-09 17:28:36 -0500

    move page flicker snippet out of in-repo-addon, as the in-repo-addon ensures the snippet is run in the head tag, and that is necessary only for websites on fastboot. For websites not on fastboot, didRender must be overwritten, and experiments must be activated by a custom event.

--- app/instance-initializers/google-tag-manager.js
+++ app/instance-initializers/google-tag-manager.js
@@ -1,8 +1,36 @@
 import config from '../config/environment';
+import { run } from '@ember/runloop';
+import Component from '@ember/component';
+import { get } from '@ember/object';
 
-export function initialize(/* appInstance */) {
+function optimize(context) {
+  const dataLayer = get(context, 'dataLayer');
+  console.log(dataLayer)
+  if (dataLayer) {
+    dataLayer.push({'event': 'optimize.activate'});
+  }
+}
+
+
+export function initialize(appInstance) {
   if (typeof window !== 'undefined' && config.environment !== 'test') {
     console.log("initializing google analytics");
+      // application.inject('route', 'foo', 'service:foo');
+      appInstance.inject('component', 'dataLayer', 'service:nypr-metrics/data-layer');
+
+      Component.reopen({
+        didRender() {
+          run.throttle(Component, optimize, this, 100);
+          return this._super(...arguments);
+        },
+      });
+
+      (function(a,s,y,n,c,h,i/*,d,e*/){s.className+=' '+y;
+    h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+    (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);
+    })(window,document.documentElement,'async-hide','dataLayer', 2000,{[config.googleOptimize]:true});
+
+
 
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
