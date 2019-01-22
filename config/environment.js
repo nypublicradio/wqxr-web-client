ee09585ec3bc7c50ade1f751cd732ee97f9d1b54
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-01-22 17:12:41 -0500

    refactor adspot to use environmental variable

--- config/environment.js
+++ config/environment.js
@@ -92,7 +92,9 @@ module.exports = function(environment) {
     },
     googleTagManager: process.env.GOOGLE_TAG_MANAGER_ID || 'GTM-MZ2S75K',
     googleOptimize: process.env.GOOGLE_OPTIMIZE_ID,
-    googleAnalytics: process.env.GOOGLE_ANALYTICS
+    googleAnalytics: process.env.GOOGLE_ANALYTICS,
+    googleAdMangerAdspot: process.env.GOOGLE_AD_MANAGER_ADSPOT
+
   };
 
   if (environment === 'development') {
