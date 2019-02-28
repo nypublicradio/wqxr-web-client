8a437edeff07de4a80224bcbbaa52f2c04bed120
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-02-28 17:09:08 -0500

    remove extraneous code

--- app/playlist/route.js
+++ app/playlist/route.js
@@ -1,10 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  beforeModel(){
-    this.transitionTo('playlist-daily')
-
-  },
 
   model({ slug }) {
     this.transitionTo('playlist-daily', { queryParams: { scheduleStation: slug } })
