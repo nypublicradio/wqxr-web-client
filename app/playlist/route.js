2b641d0d4b9d0d0ac2623c020a30d85bf1224038
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-03-01 15:24:02 -0500

    change redirect from model hook to redirect hook. fix tests.

--- app/playlist/route.js
+++ app/playlist/route.js
@@ -1,10 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-
-  model({ slug }) {
-    this.transitionTo('playlist-daily', { queryParams: { scheduleStation: slug } })
-    return;
-  },
+  redirect(model, { slug }) {
+    this.transitionTo('playlist-daily', {
+      queryParams: { scheduleStation: slug }
+    });
+  }
 
 });
--- tests/acceptance/playlist-test.js
+++ tests/acceptance/playlist-test.js
@@ -1,7 +1,9 @@
-import { findAll, currentURL, find, visit } from '@ember/test-helpers';
+import { findAll, currentURL, visit } from '@ember/test-helpers';
 import test from 'ember-sinon-qunit/test-support/test';
 import { setupApplicationTest } from 'ember-qunit';
 import { module } from 'qunit';
+import moment from 'moment';
+
 
 module('Acceptance | playlist', function(hooks) {
   setupApplicationTest(hooks);
@@ -20,35 +22,12 @@ module('Acceptance | playlist', function(hooks) {
         end: "2016-09-15T13:00:15.542Z" // 9 am
       }
     });
+    let date = moment().format('YYYY/MMM/DD').toLowerCase();
+    server.create('django-page', {id: `playlist-daily/${date}/?scheduleStation=wqxr`});
 
     await visit('/streams/wqxr');
 
-    assert.equal(currentURL(), '/streams/wqxr');
-    assert.equal(find('a[href="http://fooshow.com"]').textContent.trim(), 'Episode Foo');
+    assert.equal(currentURL(), `/playlist-daily/${date}?scheduleStation=wqxr`);
     assert.equal(findAll('.leaderboard-ad').length, 1, 'leaderboard is present');
-    assert.equal(findAll('#rightRail').length, 1, 'sidebar ad is present');
-  });
-
-  test('playlist routes do dfp targeting', async function() /*assert*/{
-    server.create('bucket', {slug: 'wqxr-home'});
-    server.create('stream', {
-      slug: 'wnyc-fm939',
-      name: 'WNYC FM',
-    });
-    server.create('whats-on', {
-      slug: 'wnyc-fm939',
-      current_show: {
-        show_title: 'Foo Show',
-        title: 'Episode Foo',
-        url: 'http://fooshow.com',
-        end: "2016-09-15T13:00:15.542Z" // 9 am
-      }
-    });
-
-    this.mock(this.owner.lookup('route:playlist').get('googleAds'))
-      .expects('doTargeting')
-      .once();
-
-    await visit('/streams/wnyc-fm939');
   });
 });
