55268be98c2b7722651ea36291cc123ef123f327
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-04-12 10:34:15 -0400

    fastboot guardrails, modify full-bleed template to just show loading screen on initial fastboot load.

--- app/routes/videos.js
+++ app/routes/videos.js
@@ -1,11 +1,19 @@
 import Route from '@ember/routing/route';
+import { reads } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 
 export default Route.extend({
+  fastboot: service(),
+  isFastBoot: reads('fastboot.isFastBoot'),
+
   titleToken(model) {
-    return model.get('title');
+    return this.get('title', model);
   },
   model() {
+    if (this.get('isFastBoot')) {
+      return {'title': ''}
+    }
     return this.store.findRecord('django-page', 'videos/');
   }
 });
