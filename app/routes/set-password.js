981b79f3459f204c510231d1f56f0947c44dc1f4
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-04-19 18:00:44 -0400

    fix but that allows anyone to visit set password page

--- app/routes/set-password.js
+++ app/routes/set-password.js
@@ -1,7 +1,15 @@
 import Route from '@ember/routing/route';
+import { get } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  titleToken: 'Set Password',
+  session: service(),
+
+  beforeModel() {
+    if (get(this, 'session.isAuthenticated') || !this.get('username')) {
+      this.transitionTo('/');
+    }
+  },
 
   actions: {
     didTransition() {
