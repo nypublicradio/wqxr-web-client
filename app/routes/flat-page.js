2aef99aa4f38f671de9d5c44a4d6158f61a7c600
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-05-09 15:25:19 -0400

    use flatpage api to render flatpages, rather than django

---
+++ app/routes/flat-page.js
@@ -0,0 +1,48 @@
+import Route from '@ember/routing/route';
+import { get } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default Route.extend({
+  dataLayer: service('nypr-metrics/data-layer'),
+  metadata: service(),
+  titleToken(model) {
+    return model ? get(model, 'title') : '';
+  },
+  pageType: 'General Flatpage',
+  model({ flatpage_path }) {
+    return this.store.query('flat-page', {
+        filter: {
+          url: '/' + flatpage_path
+        }
+      }).then(function(flatPages) {
+        return flatPages.get("firstObject");
+      }).catch((err) => {
+        if (!err.isAdapterError) {
+          throw(err);
+        } else {
+          this.transitionTo('404', err.url);
+        }
+      });
+  },
+  afterModel(model, transition) {
+    this.get('metadata').setHeadData({
+      path: get(transition, 'intent.url'),
+    });
+  },
+  setupController(controller, model) {
+    this._super(...arguments);
+    controller.set('flatPage', model);
+  },
+  actions: {
+    didTransition() {
+      if (window && window.document) {
+        this.get('dataLayer').push('Page Type', this.get('pageType'));
+      }
+      return true;
+    },
+    willTransition() {
+      this.get('dataLayer').clear('Page Type');
+      return true;
+    }
+  }
+});
