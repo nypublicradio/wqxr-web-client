2b9f6f6cf5698b33c023582103d9c780827560b5
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-06-06 16:01:38 -0400

    don't render validate page in fastboot. use branch instead of commit fo r nypr-auth.

--- app/routes/validate.js
+++ app/routes/validate.js
@@ -1,9 +1,22 @@
 import Route from '@ember/routing/route';
 import DeauthenticatedRouteMixin from 'wqxr-web-client/mixins/deauthenticated-route-mixin';
+import { reads } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(DeauthenticatedRouteMixin, {
+  fastboot: service(),
+  isFastBoot: reads('fastboot.isFastBoot'),
   titleToken: 'Validate',
-  
+
+  setupController() {
+    // don't render this page in fastboot. It causes a double confirmation when
+    // page loads.
+    if (this.get('isFastBoot')){
+      return;
+    }
+    return this._super(...arguments);
+  },
+
   actions: {
     didTransition() {
       this.send('disableChrome');
