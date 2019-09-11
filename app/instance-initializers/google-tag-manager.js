efd69d8c3bfdd0138e13d918317bb012cfcc3f47
Jim Jazwiecki <jjazwiecki@nypublicradio.org>
2019-09-11 17:43:02 -0400

    Google Optimize complains about this anti-flicker script, now handled by lib/google-optimize/index.js

--- app/instance-initializers/google-tag-manager.js
+++ app/instance-initializers/google-tag-manager.js
@@ -30,13 +30,6 @@ export function initialize(appInstance) {
   });
 
   if (typeof window !== 'undefined' && typeof document !== 'undefined' && config.environment !== 'test') {
-      (function(a,s,y,n,c,h,i/*,d,e*/){s.className+=' '+y;
-    h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
-    (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);
-    })(window,document.documentElement,'async-hide','dataLayer', 2000,{[config.googleOptimize]:true});
-
-
-
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
