3fad58c9fe6783c6c20c91a2f198b6dc520b2f71
Seori <seori@gmail.com>
2019-12-18 12:50:12 -0500

    Cancel button

--- app/components/onboarding-message/component.js
+++ app/components/onboarding-message/component.js
@@ -1,10 +1,25 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
+const CANCELLEDCOOKIE = 'onboardingCancelled'
 
 export default Component.extend({
+  cookies        : service(), 
+
+  showOnboardingMessage: false, 
+
+  init() {
+    this._super(...arguments); //calls the default init method for the component class(since it's extending and we're overwriting it with this)
+    if (!this.get('cookies').exists(CANCELLEDCOOKIE)) {
+       this.set('showOnboardingMessage', true);
+    }
+
+  }, 
 
   actions: {
     hideOnboardMessage() {
-      this.onHideOnbaordMessage();
+      this.get('cookies').write(CANCELLEDCOOKIE, true);
+      this.set('showOnboardingMessage', false);
     }
   }
 });
