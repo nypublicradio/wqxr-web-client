4144961e8f11ff57967083d0e3634a8ece210958
Seori <seori@gmail.com>
2020-01-21 16:23:37 -0500

    Code hygiene and test fixes

--- tests/integration/components/jukebox-onboarding-message/component-test.js
+++ tests/integration/components/jukebox-onboarding-message/component-test.js
@@ -7,20 +7,9 @@ module('Integration | Component | onboarding-message', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{jukebox-onboarding-message}}`);
 
     assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#jukebox-onboarding-message}}
-        template block text
-      {{/jukebox-onboarding-message}}
-    `);
-
     assert.dom('[data-test-element="title"]').hasText('You\'re invited...');
     assert.dom('[data-test-element="body"]').hasText('...to our new, experimental WQXR.org! We want to hear what you think.');
     assert.dom('[data-test-element="button"]').hasText('Visit The New WQXR.org');
