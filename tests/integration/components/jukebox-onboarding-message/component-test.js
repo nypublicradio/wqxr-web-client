5b4da40b818d1fa63a8102efeccbefe7f6bd2b10
Seori <seori@gmail.com>
2020-01-24 14:58:22 -0500

    Working tests

--- tests/integration/components/jukebox-onboarding-message/component-test.js
+++ tests/integration/components/jukebox-onboarding-message/component-test.js
@@ -3,14 +3,13 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | onboarding-message', function(hooks) {
+module('Integration | Component | jukebox-onboarding-message', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     await render(hbs`{{jukebox-onboarding-message}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
     assert.dom('[data-test-element="title"]').hasText('You\'re invited...');
+    assert.dom('.onboarding-title').hasText('You\'re invited...');
     assert.dom('[data-test-element="body"]').hasText('...to our new, experimental WQXR.org! We want to hear what you think.');
     assert.dom('[data-test-element="button"]').hasText('Visit The New WQXR.org');
   });
