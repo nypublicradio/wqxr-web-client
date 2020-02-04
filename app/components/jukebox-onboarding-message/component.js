01dd0e4dade9c9e5e0707a1ae6dcbffd55bf682d
Seori <seori@gmail.com>
2020-02-04 11:52:46 -0500

    Resolving cookies, fastboot presence don't show

--- app/components/jukebox-onboarding-message/component.js
+++ app/components/jukebox-onboarding-message/component.js
@@ -1,18 +1,22 @@
 import Component from '@ember/component';
+import { reads } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 const CANCELLED_COOKIE = 'onboardingCancelled'
 
 export default Component.extend({
-  cookies        : service(),
-  showOnboardingMessage: false, 
+  cookies: service(),
+  fastboot: service(),
+  isFastboot: reads('fastboot.isFastBoot'),
+  showOnboardingMessage: false,
 
   init() {
     this._super(...arguments);
-    if (!this.get('cookies').exists(CANCELLED_COOKIE)) {
-       this.set('showOnboardingMessage', true);
+    if (this.isFastBoot || this.get('cookies').exists(CANCELLED_COOKIE)) {
+      return false;
     }
-  }, 
+    this.set('showOnboardingMessage', true);
+  },
 
   actions: {
     hideOnboardMessage() {
