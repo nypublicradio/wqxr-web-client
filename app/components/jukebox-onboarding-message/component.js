f424a011e7e1b44aa43a26422081f674ed0156cf
msbeethoven <1966606+msbeethoven@users.noreply.github.com>
2020-02-05 15:07:31 -0500

    Seori/promo qa (#293)
    * Moved promo under the search and padding
    
    * Spacing issues
    
    * Resolving cookies, fastboot presence don't show
    
    * Took out isfastboot shortcut for object
    
    * fastBoot instead of fastboot
    
    * Finished up the cookies bug + made break point bigger to avoid ugly text smushing
    
    * Remove comments and fix onboarding-body padding issue
    
    * Small corrections -- padding, pixel corrections, space removal etc

--- app/components/jukebox-onboarding-message/component.js
+++ app/components/jukebox-onboarding-message/component.js
@@ -4,15 +4,17 @@ import { inject as service } from '@ember/service';
 const CANCELLED_COOKIE = 'onboardingCancelled'
 
 export default Component.extend({
-  cookies        : service(),
-  showOnboardingMessage: false, 
+  cookies: service(),
+  fastboot: service(),
+  showOnboardingMessage: false,
 
   init() {
     this._super(...arguments);
-    if (!this.get('cookies').exists(CANCELLED_COOKIE)) {
-       this.set('showOnboardingMessage', true);
+    if (this.get('fastboot.isFastBoot') || this.get('cookies').exists(CANCELLED_COOKIE)) {
+      return false;
     }
-  }, 
+    this.set('showOnboardingMessage', true);
+  },
 
   actions: {
     hideOnboardMessage() {
