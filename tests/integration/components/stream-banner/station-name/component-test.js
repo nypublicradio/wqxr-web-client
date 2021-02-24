bd4f151fa88ed9415bd265377276e2a955046cd1
Brad Kammin <bkammin@nypublicradio.org>
2021-02-24 16:50:33 -0500

    WQXR-181 override station name component

--- tests/integration/components/stream-banner/station-name/component-test.js
+++ tests/integration/components/stream-banner/station-name/component-test.js
@@ -3,11 +3,12 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | stream-banner/trigger', function(hooks) {
+module('Integration | Component | stream-banner/station-name', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    await render(hbs`{{stream-banner/trigger}}`);
-    assert.equal(this.element.textContent.trim(), '+ More Streams');
+    this.set('activeStream', { 'name': 'WQXR 105.9' });
+    await render(hbs`{{stream-banner/station-name}}`);
+    assert.equal(this.element.textContent.trim(), 'WQXR 105.8');
   });
 });
