ba8331ab5127330a664c10a1f6e9342d527938f0
Brad Kammin <bkammin@nypublicradio.org>
2021-02-25 12:46:23 -0500

    WQXR-181 stubbed in logic for sound animatino

--- app/components/stream-banner/station-name/component.js
+++ app/components/stream-banner/station-name/component.js
@@ -1,4 +1,12 @@
 import StationName from 'nypr-audio-services/components/stream-banner/station-name'
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default StationName.extend({
+    activeStream: null,
+    hifi: service(),
+    isPlaying: computed('hifi.isPlaying', 'activeStream', function() {
+        return this.get('hifi.isPlaying') &&
+               this.get('hifi.currentSound.metadata.contentId') === this.get('activeStream.slug');
+    })
 });
