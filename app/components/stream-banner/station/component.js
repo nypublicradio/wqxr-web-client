3da7e491c0f6a09f27f1c07195d195aab6bde994
Brad Kammin <bkammin@nypublicradio.org>
2021-03-04 06:41:25 -0500

    WQXR-181 include stream name in dropdown trigger

---
+++ app/components/stream-banner/station/component.js
@@ -0,0 +1,4 @@
+import Station from 'nypr-audio-services/components/stream-banner/station'
+
+export default Station.extend({
+});
--- tests/integration/components/stream-banner/dropdown/component-test.js
+++ tests/integration/components/stream-banner/dropdown/component-test.js
@@ -3,12 +3,12 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | stream-banner/trigger', function(hooks) {
+module('Integration | Component | stream-banner/dropdown', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     this.set('stream', { 'name': 'WQXR 105.9' });
-    await render(hbs`{{stream-banner/trigger extra=stream}}`);
+    await render(hbs`{{stream-banner/dropdown activeStream=stream}}`);
     assert.dom('.stream-banner__active-stream').hasText('WQXR 105.9');
     assert.dom('.more-streams-label').hasText('+ More Streams');
   });
